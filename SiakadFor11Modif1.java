b02789fbd6fdd39b2f6fe0ee3af10945455e0141
gempitadini <gempitadini05@gmail.com>
2024-10-11 08:47:54 +0700

    percobaan modif 1

---
+++ SiakadFor11Modif1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SiakadFor11Modif1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
+
+        for (int i = 1; i <= 10; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
+        }
+    System.out.println("Nilai tertinggi: " + tertinggi); 
+    System.out.println("Nilai terendah: " + terendah);
+    System.out.println("Banyaknya mahasiswa lulus: " + lulus);
+    System.out.println("Banyaknya mahasiswa tidak lulus: " + tidakLulus);
+    sc.close();
+    }
+}
